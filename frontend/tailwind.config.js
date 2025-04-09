b9c0d2309215ee82be6ce4b310000968a3a5ad0a
jakubkucera <jakub.kucera.01@gmail.com>
2025-04-09 12:54:47 +0200

    react vite, tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    darkMode: 'class', // Povolit tmavý režim pomocí třídy
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
